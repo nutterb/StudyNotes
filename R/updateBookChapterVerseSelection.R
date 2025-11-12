946261202484e20ad19ec6d02d49afa10e054fbc
Benjamin <benjamin.nutter@gmail.com>
2025-11-11 19:02:49 -0500

    Allow searching across multiple tomes, books, and chapters. (Issue #3)

--- R/updateBookChapterVerseSelection.R
+++ R/updateBookChapterVerseSelection.R
@@ -1,46 +1,63 @@
 updateBookSelection <- function(tome_oid = 1, 
                                 session = getDefaultReactiveDomain(), 
                                 inputId = "sel_reference_book", 
-                                with_any = FALSE){
-  ThisBook <- BOOK[BOOK$ParentTome == tome_oid, ]
+                                with_any = FALSE, 
+                                selected = character(0)){
+  ThisBook <- 
+    BOOK[BOOK$ParentTome %in% tome_oid, ]
+  
   sel_book <- c(if (with_any) -1 else numeric(0), 
                 ThisBook$OID)
   names(sel_book) <- c(if (with_any) "Any" else numeric(0), 
                        ThisBook$Title)
+
+  this_sel <- selected[selected %in% sel_book]
   
   updateSelectInput(session = session, 
                     inputId = inputId, 
                     choices = sel_book, 
-                    selected = sel_book[1])
+                    selected = this_sel)
   sel_book
 }
 
 updateChapterSelection <- function(book_oid = 1, 
                                    session = getDefaultReactiveDomain(), 
                                    inputId = "sel_reference_chapter", 
-                                   with_any = FALSE){
-  ThisChap <- CHAPTER[CHAPTER$ParentBook == book_oid, ]
-  
-  sel_chap <- c(if (with_any) -1 else numeric(0), 
-                ThisChap$OID)
-
-  names(sel_chap) <- c(if (with_any) "Any" else character(0), 
-                       ThisChap$ChapterNumber)
+                                   with_any = FALSE, 
+                                   selected = character(0)){
+  if (length(book_oid) != 1) {
+    sel_chap <- character(0)
+    selected <- character(0)
+  } else {
+    ThisChap <- CHAPTER[CHAPTER$ParentBook %in% book_oid, ]
+    
+    sel_chap <- c(if (with_any) -1 else numeric(0), 
+                  ThisChap$OID)
+    
+    names(sel_chap) <- c(if (with_any) "Any" else character(0), 
+                         ThisChap$ChapterNumber)
+  }
 
   updateSelectInput(session = session,
                     inputId = inputId,
                     choices = sel_chap, 
-                    selected = sel_chap[1])
+                    selected = selected)
 
   sel_chap
 }
 
 updateVerseSelection <- function(chapter_oid = 1, 
                                  session = getDefaultReactiveDomain(), 
-                                 inputId = "chkgrp_reference_verse"){
-  ThisVerse <- VERSE[VERSE$ParentChapter == chapter_oid, ]
-  sel_verse <- ThisVerse$OID
-  names(sel_verse) <- ThisVerse$VerseNumber
+                                 inputId = "chkgrp_reference_verse", 
+                                 selected = character(0)){
+  if (length(chapter_oid) != 1) {
+    sel_verse <- character(0)
+    selected <- character(0)
+  } else {
+    ThisVerse <- VERSE[VERSE$ParentChapter == chapter_oid, ]
+    sel_verse <- ThisVerse$OID
+    names(sel_verse) <- ThisVerse$VerseNumber
+  }
   
   updateCheckboxGroupInput(session = session, 
                            inputId = inputId, 
